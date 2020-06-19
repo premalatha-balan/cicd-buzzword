11284b2688e85b747e4f60fb13922954a539454c
premalatha-balan <plbalan@yahoo.com>
2020-06-19 09:29:39 +0100

    Added heroku deployment. Travis config also updated

---
+++ .travis/deploy_heroku.sh
@@ -0,0 +1,6 @@
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+heroku container:login
+heroku container:push web --app $HEROKU_APP_NAME
+heroku container:push web --app cicd-buzzword
+heroku container:release web --app $HEROKU_APP_NAME
+heroku container:release web --app cicd-buzzword
