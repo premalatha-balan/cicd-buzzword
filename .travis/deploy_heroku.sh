83fe16146f6e6358b157deb810c88c90803ae092
premalatha-balan <plbalan@yahoo.com>
2020-06-19 11:05:50 +0100

    heroku app name changed to see whether everything going through well

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -2,5 +2,7 @@ wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku container:login
 heroku container:push web --app $HEROKU_APP_NAME
 heroku container:push web --app cicd-buzzword
+heroku container:push web --app cicdcd-buzzword
 heroku container:release web --app $HEROKU_APP_NAME
 heroku container:release web --app cicd-buzzword
+heroku container:release web --app cicdcd-buzzword
