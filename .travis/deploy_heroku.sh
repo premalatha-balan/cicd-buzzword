539cc5804eb5d67a4340209264396908cc6740e6
premalatha-balan <plbalan@yahoo.com>
2020-06-24 12:32:08 +0100

    added cicdcd-buzzword container to see whether this will make the difference

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,4 +1,5 @@
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+heroku create cicdcd-buzzword
 heroku container:login
 heroku container:push web --app $HEROKU_APP_NAME
 heroku container:push web --app cicd-buzzword
